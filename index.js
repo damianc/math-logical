d1a61c010efe129f423b99c2fcceecc9b49a1b3a
Damian <damianczapiewski@gmail.com>
2023-11-07 01:46:21 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+(function () {
+	function bool(pq) {
+		return pq.map(Boolean);
+	}
+	
+	const ops = {
+		and(p,q) {
+			[p,q] = bool([p,q]);
+			return p && q;
+		},
+		or(p,q) {
+			[p,q] = bool([p,q]);
+			return p || q;
+		},
+		xor(p,q) {
+			[p,q] = bool([p,q]);
+			return (p || q) && !(p && q);
+		},
+		nand(p,q) {
+			[p,q] = bool([p,q]);
+			return !(p && q);
+		},
+		nor(p,q) {
+			[p,q] = bool([p,q]);
+			return !(p || q);
+		},
+		xnor(p,q) {
+			[p,q] = bool([p,q]);
+			return (p && q) || !(p || q);
+		},
+		not(p) {
+			return !p;
+		},
+		implies(p,q) {
+			[p,q] = bool([p,q]);
+			return !p || q;
+		}
+	};
+	
+	Math.logical = ops;
+})()
